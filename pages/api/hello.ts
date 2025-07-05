f14889832ea59cfd4304352952663e3b42baa142
Ari Mateus <109492002+arimateusdev@users.noreply.github.com>
2025-07-05 02:55:50 +0000

    Updataing hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 export const runtime = 'edge';
 
-export default function handler(req) {
+export default function handler(req: Request) {
   return new Response(
     JSON.stringify({ message: 'Hello World' }),
     {
